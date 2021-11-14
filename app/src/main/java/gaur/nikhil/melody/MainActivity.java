9616da52a9823e1c6f9eef4535d99edbc652c93b
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-11-15 00:28:10 +0530

    create objects of listView and string array for items

--- app/src/main/java/gaur/nikhil/melody/MainActivity.java
+++ app/src/main/java/gaur/nikhil/melody/MainActivity.java
@@ -3,12 +3,16 @@ package gaur.nikhil.melody;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
