e8a7100a151371c139601b9bcbbf7ff0acc82aa2
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-11-22 13:55:56 +0530

    create a custom Adapter method

--- app/src/main/java/gaur/nikhil/melody/MainActivity.java
+++ app/src/main/java/gaur/nikhil/melody/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -15,9 +19,12 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
 import java.security.Permission;
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -75,9 +82,39 @@ public class MainActivity extends AppCompatActivity {
         final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
         items = new String[mySongs.size()];
         for (int i = 0; i < mySongs.size(); i++) { // Add songs to items list.
-            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "");
         }
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(myAdapter);
+        /*ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(myAdapter);*/
+
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
+    }
+
+    class customAdapter extends BaseAdapter { // Custom Adapter.
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View myView = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textSong = myView.findViewById(R.id.txtsongname);
+            textSong.setSelected(true);
+            textSong.setText(items[i]);
+            return myView;
+        }
     }
 }
