f1753a18f72bc382e241a374c84d323d91b8f1ab
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-11-24 20:16:01 +0530

    Create methods for buttons, seekbar and timing process.

--- app/src/main/java/gaur/nikhil/melody/PlayerActivity.java
+++ app/src/main/java/gaur/nikhil/melody/PlayerActivity.java
@@ -1,14 +1,18 @@
 package gaur.nikhil.melody;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,11 +38,35 @@ public class PlayerActivity extends AppCompatActivity {
     static MediaPlayer mediaPlayer;
     int position;
     ArrayList<File> mySongs;
+    Thread updateseekbar;
+
+    // Method to item selected.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // method for visualizer.
+    @Override
+    protected void onDestroy() {
+        if (visualizer != null) {
+            visualizer.release();
+        }
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        // Displaying view of song.
+        getSupportActionBar().setTitle("Now Playing");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // initialize the variable by there id's.
         btnprev = findViewById(R.id.btnprev);
@@ -74,6 +102,65 @@ public class PlayerActivity extends AppCompatActivity {
         mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
         mediaPlayer.start();
 
+        // function for updating seekbar.
+        updateseekbar = new Thread() {
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+
+                while (currentPosition < totalDuration) {
+                    try {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekmusic.setProgress(currentPosition);
+                    } catch (InterruptedException | IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        seekmusic.setMax(mediaPlayer.getDuration());
+        updateseekbar.start();
+        seekmusic.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+        seekmusic.getThumb().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+
+        // If user changes the song the seekbar will automatically set to 0 for new song.
+        seekmusic.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        // Update endTime.
+        String endTime = createTime(mediaPlayer.getDuration());
+        txtsstop.setText(endTime);
+
+        // Update our current time on every second.
+        final Handler handler = new Handler();
+        final int delay = 1000;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String currentTime = createTime(mediaPlayer.getCurrentPosition());
+                txtsstart.setText(currentTime);
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+
+        // onClickListener method for play button.
         btnplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,6 +188,19 @@ public class PlayerActivity extends AppCompatActivity {
                 mediaPlayer.start();
                 btnplay.setBackgroundResource(R.drawable.ic_pause);
                 startAnimation(imageView);
+                int audioSessionId = mediaPlayer.getAudioSessionId();
+                if (audioSessionId != -1) {
+                    visualizer.setAudioSessionId(audioSessionId);
+                }
+
+            }
+        });
+
+        //next listener.
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                btnnext.performClick();
             }
         });
 
@@ -119,6 +219,37 @@ public class PlayerActivity extends AppCompatActivity {
                 mediaPlayer.start();
                 btnplay.setBackgroundResource(R.drawable.ic_pause);
                 startAnimation(imageView);
+                int audioSessionId = mediaPlayer.getAudioSessionId();
+                if (audioSessionId != -1) {
+                    visualizer.setAudioSessionId(audioSessionId);
+                }
+
+            }
+        });
+
+        // Visualizer.
+        int audioSessionId = mediaPlayer.getAudioSessionId();
+        if (audioSessionId != -1) {
+            visualizer.setAudioSessionId(audioSessionId);
+        }
+
+        // onClickListener method for fast forward button.
+        btnff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 10000);
+                }
+            }
+        });
+
+        // onClickListener method for fast rewind button.
+        btnfr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 10000);
+                }
             }
         });
 
@@ -131,5 +262,18 @@ public class PlayerActivity extends AppCompatActivity {
         AnimatorSet animatorSet  = new AnimatorSet();
         animatorSet.playTogether(animator);
         animatorSet.start();
+    }
+
+    // time method.
+    public String createTime(int duration) {
+        String time = "";
+        int min = duration/1000/60;
+        int sec = duration/1000%60;
+        time += min+ ":";
+        if (sec < 10) {
+            time+="0";
+        }
+        time += sec;
+        return time;
     }
 }
